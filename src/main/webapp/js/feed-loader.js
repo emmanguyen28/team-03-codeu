a1cb5437031babfe5bc87eb5eeb000413c5b782f
donatKapesa <donatkap14@gmail.com>
2019-06-29 21:58:09 -0400

    fixed little bug where images wouldn't display on the feed page

--- src/main/webapp/js/feed-loader.js
+++ src/main/webapp/js/feed-loader.js
@@ -20,40 +20,55 @@ function fetchMessages() {
 
 function buildMessageDiv(message) {
 
-    const wrapper = document.createElement('div'); 
-    wrapper.classList.add('card');
-    wrapper.style = 'width: 70rem'; 
+	const wrapper = document.createElement('div');
+	wrapper.classList.add('card');
+	wrapper.style = 'width: 70rem';
 
-    const inner_wrapper = document.createElement('div'); 
-    inner_wrapper.classList.add('card-body'); 
-    wrapper.appendChild(inner_wrapper); 
+	const inner_wrapper = document.createElement('div');
+	inner_wrapper.classList.add('card-body');
+	wrapper.appendChild(inner_wrapper);
 
-    const card_title = document.createElement('h4');
-    card_title.classList.add('card-title'); 
-    card_title.appendChild(document.createTextNode(message.user)); 
-    inner_wrapper.appendChild(card_title); 
+	const card_title = document.createElement('h4');
+	card_title.classList.add('card-title');
+	card_title.appendChild(document.createTextNode(message.user));
+	inner_wrapper.appendChild(card_title);
 
-    const card_list = document.createElement('ul');
-    card_list.classList.add('list-group');
-    card_list.classList.add('list-group-flush'); 
-    wrapper.appendChild(card_list); 
+	const card_list = document.createElement('ul');
+	card_list.classList.add('list-group');
+	card_list.classList.add('list-group-flush');
+	wrapper.appendChild(card_list);
 
-    const time_li = document.createElement('li'); 
-    time_li.classList.add('list-group-item'); 
-    time_li.appendChild(document.createTextNode(new Date(message.timestamp))); 
-    card_list.appendChild(time_li);
+	const time_li = document.createElement('li');
+	time_li.classList.add('list-group-item');
+	time_li.appendChild(document.createTextNode(new Date(message.timestamp)));
+	card_list.appendChild(time_li);
 
-    const message_li = document.createElement('li'); 
-    message_li.classList.add('list-group-item'); 
-    message_li.appendChild(document.createTextNode(message.text)); 
-    card_list.appendChild(message_li); 
+	const message_li = document.createElement('li');
+	message_li.classList.add('list-group-item');
+	var messageText = convertImageAddressToAnchorTag(message.text);
+	
+	message_li.innerHTML = messageText;
 
-    return wrapper;
-}   
+	const imageUrl = message.imageUrl;
+	
+	// 1st check checks for null, undefined, empty strings
+	// 2nd check check if string is made up  of only white spaces
+	if (Boolean(imageUrl) && !!imageUrl.trim()) {
+		console.log('inside if');
+		const image = document.createElement('img');
+		image.src = imageUrl;
+		console.log(image);
+		message_li.appendChild(image);
+	}
+	
+	card_list.appendChild(message_li);
+
+	return wrapper;
+}
 
 
 /** Replace image links with the img HTML tag*/
-function replaceImageAddressWithAnchorTag(text) {
+function convertImageAddressToAnchorTag(text) {
 	const regex = /(https?:\/\/.*\.(?:png|jpg))/i;
 	const replacement = '<a href="$&" target="_blank">$&</a>';
 	const result = text.replace(regex, replacement);
