3b7bf0b675f8765e38b7054bb4defab01f6ddf18
nykekana <45674574+nykekana@users.noreply.github.com>
2019-07-10 01:17:47 -0400

    commented out everything

--- src/main/java/com/google/codeu/servlets/ProfileServlet.java
+++ src/main/java/com/google/codeu/servlets/ProfileServlet.java
@@ -1,83 +1,107 @@
-package com.google.codeu.servlets;
+// /* package com.google.codeu.servlets;
 
-import java.io.IOException;
-import java.util.List;
+// import java.io.IOException;
+// import java.util.List;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.annotation.WebServlet;
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
 
-import com.google.codeu.data.Datastore;
-import com.google.codeu.data.Profile;
-import com.google.gson.Gson;
+// import com.google.codeu.data.Datastore;
+// import com.google.codeu.data.Profile;
+// import com.google.gson.Gson;
 
-/**
- * Handle fetching all messages for the public feed
- */
-@WebServlet("/updateName")
-public class NameServlet extends HttpServlet {
 
-    private Datastore datastore; 
+// /**
+//  * Handle fetching all messages for the public feed
+//  */
+// @WebServlet("/updateName")
+// public class NameServlet extends HttpServlet {
 
-    @Override
-    public void init() {
-        datastore = new Datastore(); 
-    }
+//     private Datastore datastore; 
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
+//     @Override
+//     public void init() {
+//         datastore = new Datastore(); 
+//     }
 
-        List<Message> messages = datastore.getAllMessages(); 
-        Gson gson = new Gson(); 
-        String json = gson.toJson(messages); 
+//     @Override
+//     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//         response.setContentType("application/json");
 
-        response.getOutputStream().println(json);
-	}
+//         List<Message> messages = datastore.getAllMessages(); 
+//         Gson gson = new Gson(); 
+//         String json = gson.toJson(messages); 
+
+//         response.getOutputStream().println(json);
+// 	}
 	
-}
+// }
 
-@WebServlet("/updateUsername")
-public class NameServlet extends HttpServlet {
+// @WebServlet("/updateUsername")
+// public class NameServlet extends HttpServlet {
 
-    private Datastore datastore; 
+//     private Datastore datastore; 
 
-    @Override
-    public void init() {
-        datastore = new Datastore(); 
-    }
+//     @Override
+//     public void init() {
+//         datastore = new Datastore(); 
+//     }
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+//     @Override
+//     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//         response.setContentType("text/html");
 
-        List<Message> messages = datastore.getAllMessages(); 
-        Gson gson = new Gson(); 
-        String json = gson.toJson(messages); 
+//         List<Message> messages = datastore.getAllMessages(); 
+//         Gson gson = new Gson(); 
+//         String json = gson.toJson(messages); 
 
-        response.getOutputStream().println(json);
-    }
-}
+//         response.getOutputStream().println(json);
+// 	}
+	
+// 	/** Stores a new {@link Message}. */
+// 	@Override
+// 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-@WebServlet("/updateInterests")
-public class NameServlet extends HttpServlet {
+// 		UserService userService = UserServiceFactory.getUserService();
+// 		if (!userService.isUserLoggedIn()) {
+// 			response.sendRedirect("/index.html");
+// 			return;
+// 		}
+//     	String user = userService.getCurrentUser().getEmail();
+// 		String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+// 		// Get the URL of the image the user uploaded on the Blobstore
+// 		String imageUrls = getUploadedFileUrl(request, "image");
+// 		System.out.println(imageUrls);
+		
+// 		// if imageUrls is null, it's saved like that. Will be taken care of on the front end
+// 		Message message = new Message(user, text, imageUrls);
+// 		datastore.storeMessage(message);
 
-    private Datastore datastore; 
+// 		response.sendRedirect("/user-page.html?user=" + user);
+// 	}
+// }
 
-    @Override
-    public void init() {
-        datastore = new Datastore(); 
-    }
+// @WebServlet("/updateInterests")
+// public class NameServlet extends HttpServlet {
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
+//     private Datastore datastore; 
 
-        List<Message> messages = datastore.getAllMessages(); 
-        Gson gson = new Gson(); 
-        String json = gson.toJson(messages); 
+//     @Override
+//     public void init() {
+//         datastore = new Datastore(); 
+//     }
 
-        response.getOutputStream().println(json);
-    }
-}
+//     @Override
+//     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//         response.setContentType("application/json");
+
+//         List<Message> messages = datastore.getAllMessages(); 
+//         Gson gson = new Gson(); 
+//         String json = gson.toJson(messages); 
+
+//         response.getOutputStream().println(json);
+//     }
+// }
+//  */
