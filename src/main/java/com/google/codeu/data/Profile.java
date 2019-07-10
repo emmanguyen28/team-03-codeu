f4068919b44f91caaa3dd46c63a99f76b6f4b33d
nykekana <45674574+nykekana@users.noreply.github.com>
2019-07-09 23:56:13 -0400

    changes to discard

---
+++ src/main/java/com/google/codeu/data/Profile.java
@@ -0,0 +1,46 @@
+package com.google.codeu.data;
+
+
+import java.util.UUID;
+
+public class Profile {
+
+    private String name; 
+    private String username;
+    private String profile_pic;
+    private UUID id;
+    private String [] interests; 
+
+    public Profile(String name, String username, String profile_pic_URL, String [] interests) {
+        this(UUID.randomUUID(), String name, String username, String profile_pic_URL, String [] interests) {
+
+    }
+
+	public Profile(UUID id, String name, String username, String profile_pic_URL, String [] interests) {
+        this.id = id;
+        this.name = name; 
+        this.username = username;
+        this.profile_pic = profile_pic_URL;
+        this.interests = interests;
+    }
+    
+    public UUID getID(){
+        return this.id;
+    }
+
+	public String getName(){
+        return this.name;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+
+    public String getProfilePic(){
+        return this.profile_pic;
+    }
+
+    public String [] getInterests(){
+        return this.interests;
+    }
+
+}
---
+++ src/main/java/com/google/codeu/servlets/ProfileServlet.java
@@ -0,0 +1,83 @@
+package com.google.codeu.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.Profile;
+import com.google.gson.Gson;
+
+/**
+ * Handle fetching all messages for the public feed
+ */
+@WebServlet("/updateName")
+public class NameServlet extends HttpServlet {
+
+    private Datastore datastore; 
+
+    @Override
+    public void init() {
+        datastore = new Datastore(); 
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+
+        List<Message> messages = datastore.getAllMessages(); 
+        Gson gson = new Gson(); 
+        String json = gson.toJson(messages); 
+
+        response.getOutputStream().println(json);
+	}
+	
+}
+
+@WebServlet("/updateUsername")
+public class NameServlet extends HttpServlet {
+
+    private Datastore datastore; 
+
+    @Override
+    public void init() {
+        datastore = new Datastore(); 
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        List<Message> messages = datastore.getAllMessages(); 
+        Gson gson = new Gson(); 
+        String json = gson.toJson(messages); 
+
+        response.getOutputStream().println(json);
+    }
+}
+
+@WebServlet("/updateInterests")
+public class NameServlet extends HttpServlet {
+
+    private Datastore datastore; 
+
+    @Override
+    public void init() {
+        datastore = new Datastore(); 
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+
+        List<Message> messages = datastore.getAllMessages(); 
+        Gson gson = new Gson(); 
+        String json = gson.toJson(messages); 
+
+        response.getOutputStream().println(json);
+    }
+}
