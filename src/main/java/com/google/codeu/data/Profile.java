e16736cba3709628c6336e8f8155f32eb26692ac
nykekana <45674574+nykekana@users.noreply.github.com>
2019-07-14 00:16:06 -0400

    fixed profile constructor

--- src/main/java/com/google/codeu/data/Profile.java
+++ src/main/java/com/google/codeu/data/Profile.java
@@ -21,12 +21,12 @@ public class Profile {
     }
 
     public Profile(String name, String email){
-        this(UUID.randomUUID(), name, '', '','', email); 
+        this(UUID.randomUUID(), name, "", "","", email); 
        
     }
 
     public Profile(String name, String username, String email){
-        this(UUID.randomUUID(), name, username, '','', email);         
+        this(UUID.randomUUID(), name, username, "","", email);         
     }
 
 
