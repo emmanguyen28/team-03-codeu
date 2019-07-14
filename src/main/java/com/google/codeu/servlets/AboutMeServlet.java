302d65c1c35d234b394bef16327a815896fffa19
nykekana <45674574+nykekana@users.noreply.github.com>
2019-07-14 00:21:54 -0400

    changed jsoup

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -92,7 +92,7 @@ public class AboutMeServlet extends HttpServlet {
     String userEmail = userService.getCurrentUser().getEmail();
 
     String name =  Jsoup.clean(request.getParameter("user-name"), Whitelist.none()) ; 
-    String username =  Jsoup.clean(request.getParameter("user-username"), Whitelist.none()) ; 
+    // String username =  Jsoup.clean(request.getParameter("user-username"), Whitelist.none()) ; 
 
     // String name =  Jsoup.clean(request.getParameter("about-me"), Whitelist.none()) ;   
     // String name =  Jsoup.clean(request.getParameter("about-me"), Whitelist.none()) ;   
